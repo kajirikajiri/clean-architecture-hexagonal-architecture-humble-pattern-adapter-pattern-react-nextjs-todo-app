1ab787d435eddf8d47dbc18c72a9b700acf1f2b3
kajirikajiri <kajirikajiri@gmail.com>
2022-04-04 10:02:32 +0900

    wip

--- src/presenters/index/controller/index.test.tsx
+++ src/presenters/index/controller/index.test.tsx
@@ -4,29 +4,54 @@ import { todoInteractor } from "src/core/usecases/interactors/todo"
 import { mockTodoRepository as todoRepository } from "src/core/frameworksAndDrivers/mock/todo"
 import { useState } from 'react'
 
-test(`sample`, () => {
-const Example = () => {
-    const [state, dispatch] = useController()
-    const [executed, setExecuted] = useState(false)
-    if (state.todos === undefined) {
-        dispatch({
-            type: 'onload',
-            payload: {
-                todos: [{id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: ''}]
-            },
-            meta: {
-                dispatch,
-                todoInteractor,
-                todoRepository,
-            }
-        })
+describe(`onload`, () => {
+    const Example = () => {
+        const [state, dispatch] = useController()
+        if (state.todos === undefined) {
+            dispatch({
+                type: 'onload',
+                payload: {
+                    todos: [{ id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
+                },
+                meta: {
+                    dispatch,
+                    todoInteractor,
+                    todoRepository,
+                }
+            })
+        }
+        return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
     }
-    if (!executed && state.todos !== undefined) {
-        state.todosActions?.handleClear && state.todosActions.handleClear('1')
-        setExecuted(true)
+    test(`描画される値が正しいこと`, () => {
+        const { getByTestId } = render(<Example />)
+        expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","title":"hoge","description":"fuga","createdAt":"","doneAt":"","updatedAt":""}]`)
+    })
+})
+describe(`handleClear`, () => {
+    const Example = () => {
+        const [state, dispatch] = useController()
+        const [executed, setExecuted] = useState(false)
+        if (state.todos === undefined) {
+            dispatch({
+                type: 'onload',
+                payload: {
+                    todos: [{ id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
+                },
+                meta: {
+                    dispatch,
+                    todoInteractor,
+                    todoRepository,
+                }
+            })
+        }
+        if (!executed && state.todos !== undefined) {
+            state.todosActions?.handleClear && state.todosActions.handleClear('1')
+            setExecuted(true)
+        }
+        return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
     }
-    return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
-}
-    const {getByTestId} = render(<Example />)
-    expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","createdAt":"","description":"","doneAt":"","title":"","updatedAt":""}]`)
+    test(`描画される値が正しいこと`, () => {
+        const { getByTestId } = render(<Example />)
+        expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","createdAt":"","description":"","doneAt":"","title":"","updatedAt":""}]`)
+    })
 })
