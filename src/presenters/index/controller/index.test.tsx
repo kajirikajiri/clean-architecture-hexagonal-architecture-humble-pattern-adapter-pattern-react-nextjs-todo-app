e7c2b7ccb29032033afab09a92b350a8fe9d3743
kajirikajiri <kajirikajiri@gmail.com>
2022-04-05 12:08:51 +0900

    wip

--- src/presenters/index/controller/index.test.tsx
+++ src/presenters/index/controller/index.test.tsx
@@ -1,93 +1,63 @@
-import { useController } from "src/presenters/index/controller"
-import {render} from '@testing-library/react'
 import { todoInteractor } from "src/core/usecases/interactors/todo"
 import { mockTodoRepository as todoRepository } from "src/core/frameworksAndDrivers/mock/todo"
-import { useState } from 'react'
-import { useEffect } from "react"
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useController } from 'src/presenters/index/controller'
 
-describe(`onload`, () => {
-    const Example = () => {
-        const [state, dispatch] = useController()
-        if (state.todos === undefined) {
-            dispatch({
-                type: 'onload',
-                payload: {
-                    todos: [{ id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
-                },
-                meta: {
-                    dispatch,
-                    todoInteractor,
-                    todoRepository,
-                }
-            })
-        }
-        return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
-    }
-    test(`描画される値が正しいこと`, () => {
-        const { getByTestId } = render(<Example />)
-        expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","title":"hoge","description":"fuga","createdAt":"","doneAt":"","updatedAt":""}]`)
+test(`onload:描画される値が正しいこと`, () => {
+    const {result} = renderHook(() => useController())
+    act(() => {
+        const [_, dispatch] =result.current
+        dispatch({
+            type: 'onload',
+            payload: {
+                todos: [{ id: '1', title: 'title', description: 'description', createdAt: '', doneAt: '', updatedAt: '' }]
+            },
+            meta: {
+                dispatch,
+                todoInteractor,
+                todoRepository,
+            }
+        })
     })
+    expect(result.current[0].todos).toEqual([{ id: '1', title: 'title', description: 'description', createdAt: '', doneAt: '', updatedAt: '' }])
 })
 
-describe(`handleClear`, () => {
-    const Example = () => {
-        const [state, dispatch] = useController()
-        const [executed, setExecuted] = useState(false)
-        if (state.todos === undefined) {
-            dispatch({
-                type: 'onload',
-                payload: {
-                    todos: [{ id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
-                },
-                meta: {
-                    dispatch,
-                    todoInteractor,
-                    todoRepository,
-                }
-            })
-        }
-        useEffect(()=>{
-            if (!executed && state.todos !== undefined) {
-                state.todosActions?.handleClear && state.todosActions.handleClear('1')
-                setExecuted(true)
+test(`handleClear:描画される値が正しいこと`, () => {
+    const {result} = renderHook(() => useController())
+    act(() => {
+        const [state, dispatch] =result.current
+        dispatch({
+            type: 'onload',
+            payload: {
+                todos: [{ id: '1', title: 'title', description: 'description', createdAt: '', doneAt: '', updatedAt: '' }]
+            },
+            meta: {
+                dispatch,
+                todoInteractor,
+                todoRepository,
             }
-        }, [state.todos])
-        return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
-    }
-    test(`描画される値が正しいこと`, () => {
-        const { getByTestId } = render(<Example />)
-        expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","createdAt":"","description":"","doneAt":"","title":"","updatedAt":""}]`)
+        })
+        state.todosActions?.handleClear && state.todosActions.handleClear('1')
     })
+    expect(result.current[0].todos).toEqual([{ id: '1', title: '', description: '', createdAt: '', doneAt: '', updatedAt: '' }])
 })
 
-describe(`handleChangeDescription`, () => {
-    const Example = () => {
-        const [state, dispatch] = useController()
-        const [executed, setExecuted] = useState(false)
-        if (state.todos === undefined) {
-            dispatch({
-                type: 'onload',
-                payload: {
-                    todos: [{ id: '1', title: 'hoge', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
-                },
-                meta: {
-                    dispatch,
-                    todoInteractor,
-                    todoRepository,
-                }
-            })
-        }
-        useEffect(()=>{
-            if (!executed && state.todos !== undefined) {
-                state.todosActions?.handleChangeDescription && state.todosActions.handleChangeDescription('1', 'new description')
-                setExecuted(true)
+test(`handleChangeDescription:描画される値が正しいこと`, () => {
+    const {result} = renderHook(() => useController())
+    act(() => {
+        const [state, dispatch] =result.current
+        dispatch({
+            type: 'onload',
+            payload: {
+                todos: [{ id: '1', title: '', description: 'fuga', createdAt: '', doneAt: '', updatedAt: '' }]
+            },
+            meta: {
+                dispatch,
+                todoInteractor,
+                todoRepository,
             }
-        }, [state.todos])
-        console.log(86,state.todos)
-        return <div data-testid="test">{state.todos && JSON.stringify(state.todos)}</div>
-    }
-    test(`描画される値が正しいこと`, () => {
-        const { getByTestId } = render(<Example />)
-        expect(getByTestId('test').innerHTML).toEqual(`[{"id":"1","createdAt":"","description":"new description","doneAt":"","title":"","updatedAt":""}]`)
+        })
+        state.todosActions?.handleChangeDescription && state.todosActions.handleChangeDescription('1', 'new description')
     })
+    expect(result.current[0].todos).toEqual([{ id: '1', title: '', description: 'new description', createdAt: '', doneAt: '', updatedAt: '' }])
 })
